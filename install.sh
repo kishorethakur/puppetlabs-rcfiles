ae3cb92f838abda621c7e16bdf1c6378ba091ee8
Federico Voges <fvoges@gmail.com>
2017-04-30 13:15:25 +0100

    Improve install script
    Before this commit, the install script was a bit crude and could destroy
    the original files under some cirmustances
    
    change the install script to try to be idempotent (try is the key word
    there). It will skip if rcfiles if it detects a symlink and will not
    overwrite existing files.
    
    add colors. everything looks better in color (except B&W movies)
    
    add message to indicate number of modified files (probably not very
    reliable though)

--- install.sh
+++ install.sh
@@ -1,39 +1,87 @@
 #!/bin/bash
 
-# Find the dotfiles dir
-DOTFILES="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd -P )"
+# Find the script dir
+SRCDIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd -P )"
+
+# ANSI COLOR CODES
+
+CL="\e[0m"
+BO="\e[1m"
+UN="\e[4m"
+BL="\e[5m"
+IN="\e[7m"
+HI="\e[8m"
+RE="\e[27m"
+
+FK="\e[30m"
+FR="\e[31m"
+FG="\e[32m"
+FY="\e[33m"
+FB="\e[34m"
+FP="\e[35m"
+FC="\e[36m"
+FW="\e[37m"
+
+BK="\e[40m"
+BR="\e[41m"
+BG="\e[42m"
+BY="\e[43m"
+BB="\e[44m"
+BP="\e[45m"
+BC="\e[46m"
+BW="\e[47m"
+
+WARNING="${IN}${FY}${BK}Warning:${CL}"
+INFO="${IN}${BK}${FG}Info   :${CL}"
 
 function create_link() {
   SOURCE="$1"
   FILE="$2"
   SUFFIX="$3"
-  if [ -h ~/.$FILE ]
+
+  if [ -h ~/.${FILE} ]
   then
-    echo "WARNING: ~/.${FILE} is a symlink. Skipping it"
-    return
+    echo -e "${WARNING} ~/.${FILE} is a symlink already. Skipping it" 2> /dev/stderr
+    return 1
   fi
-  if [ -f ~/.$FILE  -o -d ~/.$FILE ]
+  if [ -f ~/.${FILE} -o -d ~/.${FILE} ]
   then
-    mv ~/.$FILE{,.$SUFFIX}
+    if [ -e ~/.${FILE}.${SUFFIX} ]
+    then
+      echo -e "${WARNING} ~/.${FILE}.${SUFFIX} already exists. Skipping ~/.${FILE}" 2> /dev/stderr
+      return 1
+    fi
+    echo -e "${INFO} Renaming ~/.${FILE} => ~/.${FILE}.${SUFFIX}"
+    mv ~/.${FILE}{,.${SUFFIX}}
   fi
-  ln -sfn $SOURCE/$FILE ~/.$FILE
+  echo -e "${INFO} Creating symlink ${SOURCE}/${FILE} => ~/.${FILE}"
+  ln -sfn ${SOURCE}/${FILE} ~/.${FILE}
+  return $?
 }
 
+count=0
+
 # bash
-create_link $DOTFILES bashrc local
+create_link ${SRCDIR} bashrc local
+test $? == 0 && let count++
 
 # vim
-create_link $DOTFILES vimrc old
-create_link $DOTFILES vim old
+create_link ${SRCDIR} vimrc pre-rcfiles
+test $? == 0 && let count++
+create_link ${SRCDIR} vim pre-rcfiles
+test $? == 0 && let count++
 
 # screenc
-create_link $DOTFILES screenrc old
+create_link ${SRCDIR} screenrc pre-rcfiles
+test $? == 0 && let count++
 
 # tmux
-create_link $DOTFILES tmux.conf old
+create_link ${SRCDIR} tmux.conf pre-rcfiles
+test $? == 0 && let count++
 
 # gemrc
-create_link $DOTFILES gemrc old
+create_link ${SRCDIR} gemrc pre-rcfiles
+test $? == 0 && let count++
 
 
-
+echo -e "${INFO} Files modified: ${count}"
